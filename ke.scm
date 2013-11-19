1d87862c678fede77f1938820f2041ddfe65f423
shelanoir <nhatanh02@gmail.com>
2013-11-20 00:35:48 +0700

    modified:   ke.scm 	modified:   scratchpad.scm
    	Changing database scheme

--- ke.scm
+++ ke.scm
@@ -1,6 +1,6 @@
 ;;sample data
-(load "schelog.scm")
-(define %bank-yield
+;;old version
+#|(define %bank-yield
   (%rel ()
         [('client1 'excellent)]))
 
@@ -27,7 +27,73 @@
          [('last_year_sales_growth 'client1 20)]
          [('gross_profits_on_sales 'client1 45)]
          [('short_term_debt_per_annual_sales 'client1 9)]))
-   
+|# 
+;; new data scheme
+(load "schelog.scm")
+
+(define %userdb
+  (%rel ()
+        [('client1 ;a
+         '(local_currency_deposits . 30000) ;b
+         '(foreign_currency_deposits . 20000) ;c
+         '(bank_guarantees . 3000) ;d
+         '(negotiate_instruments . 5000) ;e
+         '(stocks . 9000) ;f
+         '(mortage . 12000) ;g
+         '(documents . 14000) ;h
+         '(bank-yield . excellent) ;i
+         '(requested-credit . 50000) ;k
+         'ok-profile ;l
+         '(net_worth_per_assets . 40) ;m
+         '(last_year_sales_growth . 20) ;n
+         '(gross_profits_on_sales . 45) ;p
+         '(short_term_debt_per_annual_sales . 9) ;q
+         )]))
+
+(define %ok-profile
+  (%rel (a b c d e f g h i k l m n p q)
+        [(a)
+         (%userdb a b c d e f g h i k 'ok-profile m n p q)]))
+
+(define %bank-yield
+  (%rel (yield= a b c d e f g h i k l m n p q)
+        [(a yield=)
+         (%userdb a b c d e f g h (cons 'bank-yield yield=) k l m n p q)]))
+
+(define %requested-credit
+  (%rel (credit= a b c d e f g h i k l m n p q)
+        [(a credit=)
+         (%userdb a b c d e f g h i (cons 'requested-credit credit=) l m n p q)]))
+
+(define %value
+  (%rel (a b c d e f g h i k l m n p q val)
+        [('net_worth_per_assets a val)
+         (%userdb a b c d e f g h i k l (cons 'net_worth_per_assets val) n p q)]
+        [('last_year_sales_growth a val)
+         (%userdb a b c d e f g h i k l m (cons 'last_year_sales_growth val) p q)]
+        [('gross_profits_on_sales a val)
+                  (%userdb a b c d e f g h i k l m n (cons 'gross_profits_on_sales val) q)]
+        [('short_term_debt_per_annual_sales a val)
+                           (%userdb a b c d e f g h i k l m n p (cons 'short_term_debt_per_annual_sales val))
+         ]))
+
+
+(define %amount
+  (%rel (a b c d e f g h i k l m n p q val)
+        [('local_currency_deposits a val)
+         (%userdb a (cons 'local_currency_deposits val) c d e f g h i k l m n p q)]
+        [('foreign_currency_deposits a val)
+         (%userdb a b (cons 'foreign_currency_deposits val) d e f g h i k l m n p q)]
+        [('bank_guarantees a val)
+         (%userdb a b c (cons 'bank_guarantees val) e f g h i k l m n p q)]
+        [('negotiate_instruments a val)
+         (%userdb a b c d (cons 'negotiate_instruments val) f g h i k l m n p q)]
+        [('stocks a val)
+         (%userdb a b c d e (cons 'stocks val) g h i k l m n p q)]
+        [('mortage a val)
+         (%userdb a b c d e f (cons 'mortage val) h i k l m n p q)]
+        [('documents a val)
+         (%userdb a b c d e f g (cons 'documents val) i k l m n p q)]))
 
 ;;;;
 
@@ -233,7 +299,10 @@
                     suggestion)]))
 
 
-
+(define (es:query client) (? (Client= Requested= Collateral-rating= Financial-rating= Yield= Suggestion=)
+                             (%and (%credit client Collateral-rating= Financial-rating= Yield= Suggestion=)
+                                   (%is Client= client)
+                                   (%requested-credit client Requested=))))
 
 
 
--- scratchpad.scm
+++ scratchpad.scm
@@ -84,3 +84,70 @@
          (%fact T Z)
          (%is Y (* X Z))]))
          
+
+(define %userdb
+  (%rel ()
+        [('client1 ;a
+         '(local_currency_deposits 30000) ;b
+         '(foreign_currency_deposits 20000) ;c
+         '(bank_guarantees 3000) ;d
+         '(negotiate_instruments 5000) ;e
+         '(stocks 9000) ;f
+         '(mortage 12000) ;g
+         '(documents 14000) ;h
+         '(bank-yield excellent) ;i
+         '(requested-credit 50000) ;k
+         'ok-profile ;l
+         '(net_worth_per_assets 40) ;m
+         '(last_year_sales_growth 20) ;n
+         '(gross_profits_on_sales 45) ;p
+         '(short_term_debt_per_annual_sales 9) ;q
+         )]))
+
+(define %ok-profile
+  (%rel (a b c d e f g h i k l m n p q)
+        [(a)
+         (%userdb a b c d e f g h i k 'ok-profile m n p q)]))
+
+(define %bank-yield
+  (%rel (yield= a b c d e f g h i k l m n p q)
+        [(a yield=)
+         (%userdb a b c d e f g h (cons 'bank-yield yield=) k l m n p q)]))
+
+(define %requested-credit
+  (%rel (credit= a b c d e f g h i k l m n p q)
+        [(a credit=)
+         (%userdb a b c d e f g h i (cons 'requested-credit credit=) l m n p q)]))
+
+(define %value
+  (%rel (a b c d e f g h i k l m n p q val)
+        [('net_worth_per_assets a val)
+         (%userdb a b c d e f g h i k l (cons 'net_worth_per_assets val) n p q)]
+        [('last_year_sales_growth a val)
+         (%userdb a b c d e f g h i k l m (cons 'last_year_sales_growth val) p q)]
+        [('gross_profits_on_sales a val)
+                  (%userdb a b c d e f g h i k l m n (cons 'gross_profits_on_sales val) q)]
+        [('short_term_debt_per_annual_sales a val)
+                           (%userdb a b c d e f g h i k l m n p (cons 'short_term_debt_per_annual_sales val))
+         ]))
+
+
+(define %amount
+  (%rel (a b c d e f g h i k l m n p q val)
+        [('local_currency_deposits a val)
+         (%userdb a (cons 'local_currency_deposits val) c d e f g h i k l m n p q)]
+        [('foreign_currency_deposits a val)
+         (%userdb a b (cons 'foreign_currency_deposits val) d e f g h i k l m n p q)]
+        [('bank_guarantees a val)
+         (%userdb a b c (cons 'bank_guarantees val) e f g h i k l m n p q)]
+        [('negotiate_instruments a val)
+         (%userdb a b c d (cons 'negotiate_instruments val) f g h i k l m n p q)]
+        [('stocks a val)
+         (%userdb a b c d e (cons 'stocks val) g h i k l m n p q)]
+        [('mortage a val)
+         (%userdb a b c d e f (cons 'mortage val) h i k l m n p q)]
+        [('documents a val)
+         (%userdb a b c d e f g (cons 'documents val) i k l m n p q)]))
+         
+
+(? (a b c d e f g h i k l m n p q) (%userdb a b c d e f g h i k l m n p q))
